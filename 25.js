ddb87b72f69962a31ad0e45f0d715cc8649e4eda
Андрей <Ands2909902@gmail.com>
2022-12-30 12:35:15 +0300

    25

---
+++ 25.js
@@ -0,0 +1,12 @@
+
+const reverseSeq = n => {
+    let array = n
+    sum = []
+    for(let i = 1;i <= array; i++) {
+     sum.push(i)  
+    }
+    
+    let rr = sum
+    rr.reverse()
+    return(rr);
+    };
