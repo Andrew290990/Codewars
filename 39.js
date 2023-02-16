00a74a0009d4ddc2ba59e6c14280fa04d7a36cc2
Андрей <Ands2909902@gmail.com>
2023-02-16 13:08:51 +0300

    39

---
+++ 39.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+//('person'), 'erso');
+
+
+
+
+function removeChar(str){
+
+   
+   };
+
+   let a = 'person'
+   let b = a.substring(1)
+   let c = b.slice(0,-1)
+
+
+
+
+
+   console.log(c);
